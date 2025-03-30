1fd7ce5fc0b3ab35f4495ba3b8bf7bde9a039a55
gauribirajdar22 <birajdargauri22@gamil.com>
2025-03-30 11:53:59 +0530

    Updated

--- app/src/main/java/com/example/whatsapp/presentation/communitiesscreen/CommunitiesScreen.kt
+++ app/src/main/java/com/example/whatsapp/presentation/communitiesscreen/CommunitiesScreen.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
@@ -48,6 +51,13 @@ fun CommunitiesScreen() {
     var showMenu by remember {
         mutableStateOf(false)
     }
+    val samplecommunities=listOf(
+        Communities(image=R.drawable.image,name="Tech Enthusiast",members="345 members") ,
+        Communities(image=R.drawable.image,name="Gamers",members="568 members"),
+        Communities(image=R.drawable.image,name="Travellers Community",members="234 members")
+
+
+    )
 
     Scaffold(
         topBar = {
@@ -157,8 +167,28 @@ fun CommunitiesScreen() {
         }
     ) {
         Column(modifier = Modifier.padding(it)) {
-            Button(onClick = { },colors= ButtonDefaults.buttonColors(containerColor = colorResource(id=R.color.light_green))) {
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.light_green)),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text(text = "Start a new Community", fontSize = 16.sp, fontWeight = FontWeight.Bold)
 
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "Your Communities",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+            )
+
+            LazyColumn {
+                items(samplecommunities){
+                    CommunityScreenItemDesign(communities = it)
+                }
             }
         }
     }
